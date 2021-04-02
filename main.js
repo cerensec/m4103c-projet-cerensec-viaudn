a246670b2240cfbbb7c848cb7ce933bca56b7847
Nathan VIAUD <79544144+NathanViaud@users.noreply.github.com>
2021-04-02 18:04:05 +0200

    Final

--- main.js
+++ main.js
@@ -7,9 +7,8 @@ function init(){
 
 async function setLyrics(){
 
-    //reinitHTML(longeur);
     $("#bloc-resultats").empty();
-    var search = document.getElementById("input-recherche").value;
+    let search = document.getElementById("input-recherche").value;
 
     
 
@@ -25,29 +24,22 @@ async function setLyrics(){
     }else if(!estDansLS(search)){
         $("#img-etoile").attr("src", "images/etoile-vide.svg");
     }
-    //console.log(monLocalStorage.length);
 
-    //console.log(estDansLS(search));
 
-    var url ="https://api.lyrics.ovh/v1/";              
+    // Appel de l'API
+    const url ="https://api.lyrics.ovh/v1/";              
+    let apiRes = url+search;
+    let json_lyrics = await getLyrics(apiRes);
 
-    const apiRes = url+search;
-
-    var json_lyrics = await getLyrics(apiRes);
-    //console.log(json_lyrics);
-    // var lyrics = JSON.stringify(json_lyrics);
-    // var lyricsSP;
-
-    // lyrics = lyrics.split("\\r\\n");
-    // for (let i = 0; i < lyrics.length; i++) {
-    //     lyricsSP = lyrics[i].split("\\n");       
-    // }
+    //Adaptation pour le format HTML
     str = json_lyrics.lyrics.replace(new RegExp('\r?\n','g'), '<br />');
+
     affichage(str);
     
-    //console.log(lyricsSP);
 }
 
+
+// Récuperation des lyrics
 async function getLyrics(search){
     try{
         const response = await fetch(search);
@@ -59,45 +51,32 @@ async function getLyrics(search){
 }
 
 function affichage(str){
-    // longeur = array.length;
-    // for (let i = 0; i < array.length; i++) {
-    //     $("#bloc-resultats").append($(document.createElement('p')).attr('class',"lyrics").text(array[i]));  
-    // }
     $("#bloc-resultats").empty();
     $("#bloc-resultats").append("<p>"+str+"</p>");
 }
 
-function reinitHTML(num){
-    for (let i = 0; i < num; i++) {
-        $(".lyrics").remove();
-    }
-}
 
 function addFavourite(){
     $("#img-etoile").attr("src", "images/etoile-pleine.svg");
-    //let i = monLocalStorage.length+1;
+
     let search = document.getElementById("input-recherche").value;
+
     document.getElementById("favoris-vide").style.visibility = "hidden";
+
+     //Attribution de la clé du favoris
     if(!estDansLS(search) && (search != null)){
         if(monLocalStorage.length >0){
-            console.log("fav index");
             let prevKey = monLocalStorage.key(monLocalStorage.length-1);
             let index = parseInt(prevKey.split(" ")[1])+1;
-            monLocalStorage.setItem("fav "+index, search);
+            monLocalStorage.setItem("fav "+index, search); //Création du l'objet dans localStorage
         } else{
-            console.log("fav 1");
             monLocalStorage.setItem("fav 1", search);
         }
         createFavorite(search);
     }
-    
-    // if(!estDansLS(search) && (search != null)){
-    //     monLocalStorage.setItem("fav "+i, search);
-    //     createFavorite(search);
-    // }
-
 }
 
+//Vérifacation de la présence de recherche dans localStorage
 function estDansLS(recherche){
     for (let i = 0; i < monLocalStorage.length; i++) {
         let key = monLocalStorage.key(i);
@@ -110,20 +89,23 @@ function estDansLS(recherche){
     return false;
 }
 
+// Lancement de la recherche lors du clic dans la barre des favoris
 function searchFav(elmt){
     $("#input-recherche").val(elmt.innerHTML);
     setLyrics();
-    console.log(elmt.innerHTML);
 }
 
+
 function deleteFav(elmt){
-    console.log(elmt.id);
+    //Recherche du la clé du favoris pour la suppression
     for(i =0;i<monLocalStorage.length;i++){
         let key = monLocalStorage.key(i);
         if(monLocalStorage.getItem(key) == elmt.id){
             monLocalStorage.removeItem(key);
         }
     }
+
+    //Enlever l'étoile pleine si la recherche actuelle etait la favorite supprimée
     if($("#input-recherche").val() == elmt.id){
         $("#img-etoile").attr("src", "images/etoile-vide.svg");
     }
@@ -131,6 +113,7 @@ function deleteFav(elmt){
 
 }
 
+//Création de l'objet graphique favoris dans la barre des favoris
 function createFavorite(res){
     let ul = document.getElementById("liste-favoris");
     let li = document.createElement("li");
@@ -149,8 +132,12 @@ function createFavorite(res){
     
 }
 
+
+// Affichage des favoris
 function displayFav(){
     $("#liste-favoris").empty();
+
+    //Recuperation des éléments de localStorage pour créer les favoris
     if(monLocalStorage.length > 0){
         document.getElementById("favoris-vide").style.visibility = "hidden";
         for(i = 0; i<monLocalStorage.length; i++){
@@ -160,7 +147,7 @@ function displayFav(){
             }
         }
     } else{
-        document.getElementById("favoris-vide").style.visibility = "visible";
+        document.getElementById("favoris-vide").style.visibility = "visible"; //Affichage de "∅ Aucune recherche enregistrée"
     }
     
 
